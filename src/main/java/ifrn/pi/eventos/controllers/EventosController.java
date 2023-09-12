3db780a961b781655261a68b114bd34c99696ac9
wellington-filh0 <wellingtonfilho721@gmail.com>
2023-09-11 23:47:54 -0300

    Adicionando o método que ao clicar em submit do form irá mostrar os valores no console e abrir uma nova pagína informando que deu certo

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -1,15 +1,35 @@
 package ifrn.pi.eventos.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import ifrn.pi.eventos.models.Evento;
 
 @Controller
 
 public class EventosController {
-	
+
 	@RequestMapping("/eventos/form")
 	public String form() {
 		return "formEvento";
+	}
+
+	@PostMapping("/submit")
+	public String submetido(Evento evento) {
+		System.out.println("Dados do evento:");
+		System.out.println("Nome: " + evento.getNome());
+		System.out.println("Local: " + evento.getLocal());
+		System.out.println("Data: " + evento.getData());
+		System.out.println("Horário: " + evento.getHorario());
+
+		return "redirect:/success";
+	}
+	
+	@GetMapping("/success")
+	public String successPage() {
+		return "success";
 	}
 
 }
